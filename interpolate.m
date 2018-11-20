8c9437ddd2c8b07c3764da28b335289636721e3e
WaseemWeshahi <41228348+WaseemWeshahi@users.noreply.github.com>
2018-11-20 17:22:58 +0200

    Additional subfunctions
    added additional sub functions for a cleaner and more understandable code

---
+++ interpolate.m
@@ -0,0 +1,42 @@
+function    V = interpolate(image,point)
+    % INPUT: a grey scale image
+    %        & a point on the image (that isn't necesarily an integer)
+    % OUTPUT: The inerpolated (Binlineary) value of the "point" from "image"
+    % METHOD: Perform a binlinear interpolation as shown in class where first
+    % find out the 4 points surronding "point", then perform a linear
+    % interpolation on the upper pair and the lower pairs, then we interpolate
+    % those two values
+    %
+    %
+    % Written By:
+    % Waseem Weshahi    I.D: 206943391
+    % Bayan Farhan      I.D: 208300145
+    
+    % finding out the 4 pixels surrounding the point
+    nw = floor(point);
+    nw(nw>254) = 254;
+    nw(nw<0) = 0;
+    se = [nw(1)+1;
+        nw(2)+1];
+    sw = [nw(1);
+        nw(2)+1];
+    ne = [nw(1)+1;
+        nw(2)];
+    
+    % calculating ?x and ?y
+    dX = point(1) - nw(1);
+    dY = point(2) - nw(2);
+    
+    % reading the values of the surrounding point
+    SW = image(sw(1),sw(2));
+    SE = image(se(1),se(2));
+    NW = image(nw(1),nw(2));
+    NE = image(ne(1),ne(2));
+    
+    % Lineary interpolating two pairs
+    S = SE*dX + SW*(1-dX);
+    N = NE*dX + NW*(1-dX);
+    
+    % Lineary interpolating last two results
+    V = N*dY + S*(1-dY);
+end
--- mapIntoImage.m
+++ mapIntoImage.m
@@ -2,11 +2,11 @@ function    [newImage] = mapIntoImage(imageA,imageB)
     % INPUT: two grey scale Images
     % OUTPUT: imageA into which image B has been mapped.
     % METHOD: uses inverse mapping from image A to image B in the selcted Area
+    %
+    %
     % Written By:
     % Waseem Weshahi   I.D: 206943391
     % Bayan Farhan     I.D: 208300145
-    %
-    %
 
     showImage(imageA);
     disp('please click on four points in the image, make sure they are in clockwise order and that they form a convex');
@@ -25,32 +25,8 @@ function    [newImage] = mapIntoImage(imageA,imageB)
     imageA = imageA';
     newImage = imageA;
 
-    % now we try and find the 8 parameters to define the transformation matrix
-    % Hard coding in the rearranged "xy" matrix
-    A = [x(1) y(1) 0 0 1 0 -x(1)*z(1) -y(1)*z(1);
-        0 0 x(1) y(1) 0 1 -x(1)*w(1) -y(1)*w(1)
-        x(2) y(2) 0 0 1 0 -x(2)*z(2) -y(2)*z(2)
-        0 0 x(2) y(2) 0 1 -x(2)*w(2) -y(2)*w(2)
-        x(3) y(3) 0 0 1 0 -x(3)*z(3) -y(3)*z(3)
-        0 0 x(3) y(3) 0 1 -x(3)*w(3) -y(3)*w(3)
-        x(4) y(4) 0 0 1 0 -x(4)*z(4) -y(4)*z(4)
-        0 0 x(4) y(4) 0 1 -x(4)*w(4) -y(4)*w(4)
-        ];
-
-    % merging the x' y' vector
-    combined = [z'; w'];
-    combined = combined(:);
-
-    % solving for the 8 parameters
-    parameters = pinv(A) * combined;
-
-    % storing the inverse transformation matrix
-    final_A = [parameters(1) parameters(2) parameters(5);
-        parameters(3) parameters(4) parameters(6)
-        parameters(7) parameters(8) 1];
-    final_A = pinv(final_A);
-
-
+    % now we go and find the (inverse) transformation matrix
+    transfMat = calcTransfMat(x,y,z,w);
     [width,height]=size(imageA);
     %     xvec = 1:width;
     %     yvec = 1:height;
@@ -70,43 +46,20 @@ function    [newImage] = mapIntoImage(imageA,imageB)
         vec = [insideIndX(i);
             insideIndY(i)
             1];
-        point = final_A*vec;
+        point = transfMat*vec;
         point=point./point(3);
         point(3) = []; %elimenating the Z coordinate
 
-        % Applying the Bilinear interpolation from imB to calculate the
-        % value which we should assign to the pixel in imA
-
-        % first we figure out the 4 points surroinding the source
-        % point
-        nw = floor(point);
-        nw(nw>254) = 254;
-        nw(nw<0) = 0;
-        se = [nw(1)+1;
-            nw(2)+1];
-        sw = [nw(1);
-            nw(2)+1];
-        ne = [nw(1)+1;
-            nw(2)];
-
-        % Applying Bilinear interpolation as shown in class
-        dX = point(1) - nw(1);
-        dY = point(2) - nw(2);
-
-        SW = imageB(sw(1),sw(2));
-        SE = imageB(se(1),se(2));
-        NW = imageB(nw(1),nw(2));
-        NE = imageB(ne(1),ne(2));
-
-        S = SE*dX + SW*(1-dX);
-        N = NE*dX + NW*(1-dX);
-        V = N*dY + S*(1-dY);
-
+        % Applying the Bilinear interpolation from imB on the point 
+        % to calculate the value which we should assign to the pixel in
+        % the new image
+        V = interpolate(imageB,point);
         newImage(insideIndX(i),insideIndY(i)) = fix(V); %potential bug
 
 
     end
-    % retransposing the image
+    
+    % retransposing the new image
     newImage = newImage';
     showImage(newImage);
 end
