5c3f3468a18608d35f9bab6020038b29bff78739
WaseemWeshahi <41228348+WaseemWeshahi@users.noreply.github.com>
2018-12-20 01:23:00 +0200

    Added cleanImageMean

---
+++ Assignment3/cleanImageMean.m
@@ -0,0 +1,9 @@
+function cleanIm = cleanImageMean (im, maskRadius, maskSTD)
+mask = zeros(maskRadius+3);
+[m,n] = size(mask);
+% mask = fspecial('gaussian', size(mask),maskSTD);
+[h1, h2] = meshgrid(-(m-1)/2:(m-1)/2, -(n-1)/2:(n-1)/2);
+hg = exp(- (h1.^2+h2.^2) / (2*maskSTD^2));
+mask = hg ./ sum(hg(:));
+
+cleanIm = conv2(im,mask);
--- Assignment3/denoiseScript.m
+++ Assignment3/denoiseScript.m
@@ -3,8 +3,8 @@ imA = readImage('horseBoy.tif');
 showImage(imA);
 [width,height]=size(imA);
 % imB=addSPnoise(imA,0.1);
-imB = addSPnoise(imA,0.2);
+imB = addGaussianNoise(imA,4);
 showImage(imB);
-clean = cleanImageMedian(imB,[2 2]);
+clean = cleanImageMean(imB,[2 2],4);
 showImage(clean);
 
