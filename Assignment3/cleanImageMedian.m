4000b1a346f94ec7182cceed5b943efc45a0795d
WaseemWeshahi <41228348+WaseemWeshahi@users.noreply.github.com>
2018-12-20 00:59:05 +0200

    Added Median filter

--- Assignment3/cleanImageMedian.m
+++ Assignment3/cleanImageMedian.m
@@ -1,7 +1,12 @@
 function cleanIm = cleanImageMedian (im, maskRadius)
 % Zero padding
+cleanIm=im;
 [m,n] = size(im);
  im = [[zeros(maskRadius(1),n);im] zeros(m+maskRadius(1),maskRadius(2))]; % Padding at the top and the right 
  im = [ zeros(m+2*maskRadius(1),maskRadius(2)) [im;zeros(maskRadius(1),n+maskRadius(2))] ]; % Padding at the bottom and 
- 
+ for i = maskRadius(1)+1: m+maskRadius(1)
+     for j = maskRadius(2)+1: n+maskRadius(2)
+         cleanIm(i,j) = median(im(i-maskRadius(1):i+maskRadius(1),j-maskRadius(2):j+maskRadius(2)),'all');
+     end
+ end
  
--- Assignment3/denoiseScript.m
+++ Assignment3/denoiseScript.m
@@ -3,7 +3,8 @@ imA = readImage('horseBoy.tif');
 showImage(imA);
 [width,height]=size(imA);
 % imB=addSPnoise(imA,0.1);
-imB = addGaussianNoise(imA,10);
+imB = addSPnoise(imA,0.2);
 showImage(imB);
-
+clean = cleanImageMedian(imB,[2 2]);
+showImage(clean);
 
