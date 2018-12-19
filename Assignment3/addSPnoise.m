817a7c758da9316ca69aaef19d441473215118ab
bayanf <41196313+bayanf@users.noreply.github.com>
2018-12-19 19:32:16 +0200

    added comments

--- Assignment3/addSPnoise.m
+++ Assignment3/addSPnoise.m
@@ -1,8 +1,8 @@
 function    noisyIm = addSPnoise(im, p)
-noisyIm=im;
-temp = rand(size(im));
-zero=find(temp<p/2);
-noisyIm(zero)=0;
-one=find(temp>=p/2 & temp <p);
-noisyIm(one)=255;
+noisyIm=im; % copying im into noisyIm to save the pixels that wont be changed
+temp = rand(size(im)); % creating a matrix,as same size like im, with randm values between 0 and 1
+black=find(temp<p/2);% saving the elements that have value less than p/2 in order to:
+noisyIm(black)=0;% use them to change the equivilant pixels at noisyIm color to black
+white=find(temp>=p/2 & temp <p);% saving the elements that have value less than p and greater than p/2 in order to:
+noisyIm(white)=255;% use them to change the equivilant pixels at noisyIm color to white
 
