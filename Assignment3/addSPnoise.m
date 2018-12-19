de4754e9456dc48b3229511505320bfaa8643d42
bayanf <41196313+bayanf@users.noreply.github.com>
2018-12-19 19:21:34 +0200

    SP is perfect!

--- Assignment3/addSPnoise.m
+++ Assignment3/addSPnoise.m
@@ -1,26 +1,8 @@
 function    noisyIm = addSPnoise(im, p)
-noisyIm = im;
-
-[width,height]=size(im);% the size of the matrix
-
-amount = width*height*p;%the amount of pixles that needs to be changed
-amount=floor(amount);
-
-rows = 1 + (width-1)*rand(amount);%rows is an array of size amount with random numbers between 1 and width
-cols = 1+ (height-1)*rand(amount);%cols is an array of size amount with random numbers between 1 and height
-
-loops = floor(amount/2);
-%we loop over all the even indicies of rows and cols
-for i = 1:loops
-   
-    noisyIm(round(rows(2*i)),round(cols(2*i)))=0;
-end
-
-loops = ceil(amount/2);
-%we loop over all the odd indicies of rows and cols
-
-for i = 1:loops
-    
-   noisyIm(round(rows(2*i-1)),round(cols(2*i-1)))=255;
-end
+noisyIm=im;
+temp = rand(size(im));
+zero=find(temp<p/2);
+noisyIm(zero)=0;
+one=find(temp>=p/2 & temp <p);
+noisyIm(one)=255;
 
--- Assignment3/denoiseScript.m
+++ Assignment3/denoiseScript.m
@@ -2,7 +2,7 @@
 imA = readImage('lena.tif');
 showImage(imA);
 [width,height]=size(imA);
-imB=addSPnoise(imA,0.2);
+imB=addSPnoise(imA,0.01);
 showImage(imB);
 
 
