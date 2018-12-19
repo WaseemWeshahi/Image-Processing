c827a98c9d824fda0941275a96c9df669b309607
WaseemWeshahi <41228348+WaseemWeshahi@users.noreply.github.com>
2018-12-19 21:05:38 +0200

    Added motionBlur

--- Assignment3/addMotionBlur.m
+++ Assignment3/addMotionBlur.m
@@ -1,4 +1,7 @@
 function noisyIm = addMotionBlur(im,t)
+mask = zeros(1,t);
+mask(1:t) =  1/t;
+noisyIm = conv2(im,mask,'same');
 
 
 
--- Assignment3/denoiseScript.m
+++ Assignment3/denoiseScript.m
@@ -1,9 +1,9 @@
 % The Script
-imA = readImage('lena.tif');
+imA = readImage('horseBoy.tif');
 showImage(imA);
 [width,height]=size(imA);
-imB=addSPnoise(imA,0.5);
+% imB=addSPnoise(imA,0.1);
+imB = addMotionBlur(imA,15);
 showImage(imB);
-
 
 
