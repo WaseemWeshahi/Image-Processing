46023ce69e797ae340bac0881a4eca6bf43acc09
WaseemWeshahi <41228348+WaseemWeshahi@users.noreply.github.com>
2018-12-20 00:30:00 +0200

    Update cleanImageMedian.m

--- Assignment3/cleanImageMedian.m
+++ Assignment3/cleanImageMedian.m
@@ -1,5 +1,7 @@
 function cleanIm = cleanImageMedian (im, maskRadius)
 % Zero padding
 [m,n] = size(im);
- im = [[zeros(1,n);im] zeros(m+1,1)]; % Padding at the top and the right 
- im = [ zeros(m+2,1) [im;zeros(1,n+1)] ]; % Padding at the bottom and 
+ im = [[zeros(maskRadius(1),n);im] zeros(m+maskRadius(1),maskRadius(2))]; % Padding at the top and the right 
+ im = [ zeros(m+2*maskRadius(1),maskRadius(2)) [im;zeros(maskRadius(1),n+maskRadius(2))] ]; % Padding at the bottom and 
+ 
+ 
