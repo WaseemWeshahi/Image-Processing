a9f40173663ad3c5d5bb17ab0e29dbaaea6e609b
bayanf <41196313+bayanf@users.noreply.github.com>
2018-12-20 13:55:11 +0200

    done

--- Assignment3/denoiseScript.m
+++ Assignment3/denoiseScript.m
@@ -1,5 +1,5 @@
 % The Script
-imA = readImage('lena.tif');
+imA = readImage('Alan_Turing.tif');
 showImage(imA);
 [width,height]=size(imA);
 %imB=addSPnoise(imA,0.1);
@@ -8,11 +8,14 @@ showImage(imA);
 %showImage(imB);
 %imB=addMotionBlur(imA,10);
 %showImage(imB);
-imB = addGaussianNoise(imA,50);
-showImage(imB);
+%imB = addGaussianNoise(imA,50);
+%showImage(imB);
 
-imB = cleanImageMean(imA,[2 2],4);
-showImage(imB);
-clean = sharpen(imB,[2 2],1,6);
+%imB = cleanImageMean(imA,[2 2],4);
+%showImage(imB);
+clean = sharpen(imA,[2 2],1,-1);
+calcPSNR(imA,imA);
+ans = psnr(imA,imA);
+disp(ans);
 showImage(clean);
 
