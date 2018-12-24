080510cca83298ec629f52d5648197d1d63131e6
WaseemWeshahi <41228348+WaseemWeshahi@users.noreply.github.com>
2018-12-24 16:41:43 +0200

    Update denoiseScript.m
    E is not what she's looking for, we need the MASKSTD not the MASKRADIUS

--- Assignment3/denoiseScript.m
+++ Assignment3/denoiseScript.m
@@ -49,23 +49,61 @@ disp('%%%%%%%%%%%%%%%%%%%%% C %%%%%%%%%%%%%%%%%%%%%');
 disp('In our eyes, the Salt&Pepper noise is best cleaned by median filter');
 disp('While the gaussian noise is best cleaned by averaging kernels (in this example,a gaussian filter)');
 disp('Those answers are backed up by both visual arguments (they look better these ways) and in numerical objective arguments (the PSNR is greater in each case)');
-disp('Press any key to continue...');
-pause();
-close all
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 disp('%%%%%%%%%%%%%%%%%%%%% D %%%%%%%%%%%%%%%%%%%%%');
+imA = readImage('karlMarx.tif');
+showImage(imA);
+SP = addSPnoise(imA,0.2);
+showImage(SP);
 figure
-x = linspace(1,100,100);
-x = round(x);
+x = 1:5;
 y=x;
-for i=1:100
-    y(i) = calcPSNR(cleanImageMedian(SP,diag([x(i),x(i)])),imA);
+for i=1:length(x)
+    y(i) = calcPSNR(cleanImageMedian(SP,[x(i),x(i)]),imA);
 end
 plot(x,y);
-xlabel('maskRadious');
+xlabel('maskRadius');
 ylabel('PSNR');
 title('PSNR  as a function of maskRadius');
 disp('in the following figure, you can see the PSNR values between original and median-denoised image as a function of the size of median neighborhood – maskRadius');
+pause();
+close all
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+disp('%%%%%%%%%%%%%%%%%%%%% E %%%%%%%%%%%%%%%%%%%%%');
+imA = readImage('karlMarx.tif');
+showImage(imA);
+GI = addGaussianNoise(imA,5);
+showImage(GI);
+figure
+x = 1:10;
+y=x;
+for i=1:length(x)
+    y(i) = calcPSNR(cleanImageMean(GI,[x(i),x(i)],0.8),imA);
+end
+plot(x,y);
+xlabel('maskRadius');
+ylabel('PSNR');
+title('PSNR  as a function of maskRadius');
+disp('in the following figure, you can see the PSNR values between original and main-denoised image as a function of the size of median neighborhood – maskRadius');
+disp('As we can see, the PSNR increases, since the mean square differnce between the image and its average is minimal therefor the PSNR increases alongside the maskRadius');
+pause();
+close all
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+disp('%%%%%%%%%%%%%%%%%%%%% F %%%%%%%%%%%%%%%%%%%%%');
+imA = readImage('Alan_Turing.tif');
+showImage(imA);
+imArray = zeros([size(imA),10]);
+for i=1:size(imArray,3)
+    imArray(:,:,i) = addGaussianNoise(imA,4);
+end
+meanFilt = cleanImageMean_multi(imArray);
+medianFilt = cleanImageMedian_multi(imArray);
+figure(1);
+showImage(meanFilt);
+title('image cleaned with mean');
+figure(2);
+showImage(medianFilt);
+title('image cleaned with median');
 
 
 
