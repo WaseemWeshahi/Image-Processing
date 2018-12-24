a8670b5cecf767ff1914cbfec9666b6067d9984c
WaseemWeshahi <41228348+WaseemWeshahi@users.noreply.github.com>
2018-12-24 15:19:00 +0200

    Update denoiseScript.m

--- Assignment3/denoiseScript.m
+++ Assignment3/denoiseScript.m
@@ -1,6 +1,6 @@
 % The Script
 disp('%%%%%%%%%%%%%%%%%%%%% A %%%%%%%%%%%%%%%%%%%%%');
-imA = readImage('dali.tif');
+imA = readImage('communistLeaders.tif');
 showImage(imA);
 SP = addSPnoise(imA,0.05);
 showImage(SP);
@@ -23,7 +23,7 @@ pause();
 close all
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 disp('%%%%%%%%%%%%%%%%%%%%% B %%%%%%%%%%%%%%%%%%%%%');
-imA = readImage('dali.tif');
+imA = readImage('karlMarx.tif');
 showImage(imA);
 SP = addGaussianNoise(imA,4);
 showImage(SP);
@@ -49,9 +49,24 @@ disp('%%%%%%%%%%%%%%%%%%%%% C %%%%%%%%%%%%%%%%%%%%%');
 disp('In our eyes, the Salt&Pepper noise is best cleaned by median filter');
 disp('While the gaussian noise is best cleaned by averaging kernels (in this example,a gaussian filter)');
 disp('Those answers are backed up by both visual arguments (they look better these ways) and in numerical objective arguments (the PSNR is greater in each case)');
+disp('Press any key to continue...');
 pause();
 close all
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+disp('%%%%%%%%%%%%%%%%%%%%% D %%%%%%%%%%%%%%%%%%%%%');
+figure
+x = linspace(1,100,100);
+x = round(x);
+y=x;
+for i=1:100
+    y(i) = calcPSNR(cleanImageMedian(SP,diag([x(i),x(i)])),imA);
+end
+plot(x,y);
+xlabel('maskRadious');
+ylabel('PSNR');
+title('PSNR  as a function of maskRadius');
+disp('in the following figure, you can see the PSNR values between original and median-denoised image as a function of the size of median neighborhood – maskRadius');
+
 
 
 
