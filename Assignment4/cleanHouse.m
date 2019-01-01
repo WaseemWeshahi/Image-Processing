832fbbdb08ca7726b9657c0587d038e7436ad176
WaseemWeshahi <41228348+WaseemWeshahi@users.noreply.github.com>
2019-01-01 20:22:50 +0200

    cleanHouse DONE

--- Assignment4/cleanHouse.m
+++ Assignment4/cleanHouse.m
@@ -1,46 +1,51 @@
 function cleanHouse()
+    disp('%%%%%%%%%%%%%%%%%%%%%%%%% HOUSE %%%%%%%%%%%%%%%%%%%%%%%%%');
+    disp('if we look closely at figure 1, we see that the bottom part of the');
+    disp('image is noised by gaussian noise, and that the entire image (including');
+    disp('the bottom part) is noised with horoziontal motion blur');
+    disp('therfore we have split the restoration into two parts');
+    disp('in the UPPER part we applied INVERSE FILTIRING since we know that');
+    disp('the kernel is one of motion blur, so we only needed to guess the ');
+    disp('t (length of motion blurs kernel) and lambda (prior terms constant');
+    disp('and for the BOTTOM part we used WEINERR FILTIRING since both a convulotion');
+    disp('AND gaussian noise were applied');
+    disp('so after guessing the parameters with trial and error we have reached the following');
+    disp('result shown in figure 2.');
+    %% Splitting the Image
     im = readImage('house.tif');
     imU = im; % Upper image
     imB = im;
     imU(128:256,:)=[];
     imB(1:127,:)=[];
-    showImage(imU);
-    showFFT(imU);
+    %% cleaning the Upper part (Inverse Filtering)
     G=fft2(imU);
-    t = 10;
+    t = 10; % the motion blur's kernel, 10 worked well
     mask = zeros(1,t);
     mask(1,:) = 1/t;
     h = zeros(size(G));
     h(1,1:t)=mask;
-    showFFT(h);
-    H = fft2(h);
+    H = fft2(h); % H is the FFT of the motion blur's mask
     Hstr = conj(H);
-    lambda= 0.00005;
+    lambda= 0.00005; % 0.00005 seemed to work well
+    % After setting the parameters, we apply the inverse filtering
     FU = (G.*Hstr)./(H.*Hstr+lambda);
     fu = ifft2(FU,'symmetric');
-    showImage(fu);
-
+    %% cleaning the Bottom part (Weinerr Filtering)
     G = fft2(imB);
-    mask = zeros(1,t);
-    mask(1,:) = 1/t;
-    h = zeros(size(G));
+    h = zeros(size(G)); % setting the FFT of the mask to be the same size as G's
     h(1,1:t)=mask;
-    showFFT(h);
     H = fft2(h);
-    Hstr = conj(H);
-    lambda= 0.000008;
-    F = H;
+    Hstr = conj(H);    lambda= 0.000008; % 0.000008 seemed to work well
+    F = H; % Simply setting its size..
+    % Those Loops apply Weinerr filter for each cell
     for u=1:size(imB,1)
-    for v=1:size(imB,2)
-        F(u,v) = (Hstr(u,v)*G(u,v))/ (Hstr(u,v)*H(u,v)+lambda*(u^2+v^2));
+        for v=1:size(imB,2)
+            F(u,v) = (Hstr(u,v)*G(u,v))/ (Hstr(u,v)*H(u,v)+lambda*(u^2+v^2));
+        end
     end
-    end
-
-    D=log(1+abs(F));
-    D =fftshift(D);
-    imagesc(D);
-
     f = ifft2(F,'symmetric');
+    %% concatenating the two parts and showing them to the screen
     clean = [fu;f];
+    showImage(im);
     showImage(clean);
 end
--- Assignment4/cleanWindows.m
+++ Assignment4/cleanWindows.m
@@ -1,7 +1,13 @@
 function cleanWindows()
+    disp('%%%%%%%%%%%%%%%%%%%%%%%%% WINDOWS %%%%%%%%%%%%%%%%%%%%%%%%%');
+    disp('In this image (figure 1), we see that the image is noised by Gaussian Filter');
+    disp('But, the image has many edges (e.g: wndows,wall edges,wall textures etc..');
+    disp('Therfore we can not just apply Mean filter for this kernel does not take edges into consideration');
+    disp('meaning that we have to use:');
+    disp('BILATERAL FILTIRING');
+    disp('and the result is as shown in figure 2');
     im = readImage('windows');
     showImage(im);
-    clean = bilat(im,4,5,20);
+    clean = bilat(im,4,30,10);
     showImage(clean);
-
 end
