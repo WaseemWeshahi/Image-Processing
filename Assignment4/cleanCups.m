f725cbbf7c0354f71d662d57ab5f9ec95ca682a6
WaseemWeshahi <41228348+WaseemWeshahi@users.noreply.github.com>
2019-01-01 20:39:49 +0200

    CUPS you complete

--- Assignment4/cleanCups.m
+++ Assignment4/cleanCups.m
@@ -1,24 +1,19 @@
 function cleanCups()
+    disp('%%%%%%%%%%%%%%%%%%%%%%%%% CUPS %%%%%%%%%%%%%%%%%%%%%%%%%');
+    disp('BAYAN INSERT DESCRIPTION HERE');
+    disp('figure(1) - Original Noisy image');
+    disp('figure(2) - Cleaned Image');  
     imB = readImage('cups.tif');
-    showImage(imB);showImage(imB);
+    showImage(imB);
 
-    D=log(1+abs(fft2(imB)));
-    D =fftshift(D);
-    imagesc(D);
-    %showImage(imB);
     [width,height]=size(imB);
     M = zeros(width,height);
     M(8,20)=0.5;
     %M(1,height)=-0.2;
     M(6,1)=0.5;
 
-    showImage(M);
     M = fft2(M);
-    D=log(1+abs(M));
-    D =fftshift(D);
-    imagesc(D);
-    h = find(abs(M)<0.02);
-    M(h)=1;
+    M(abs(M)<0.02)=1;
     bim = abs(ifft2(fft2(imB)./M));
     showImage(bim);
 end
