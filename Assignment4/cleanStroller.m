37ff4d04a214722001eedd518d912e56078755ab
bayanf <41196313+bayanf@users.noreply.github.com>
2019-01-02 20:05:43 +0200

    done
    please read it

--- Assignment4/cleanStroller.m
+++ Assignment4/cleanStroller.m
@@ -2,62 +2,63 @@ function cleanStroller()
     disp('%%%%%%%%%%%%%%%%%%%%%%%%% STROLLER %%%%%%%%%%%%%%%%%%%%%%%%%');
     disp('The stroller image was blurred.');
     disp('so we thought that all we need to do is to shrpen it.');
-    disp('we have tried several ways of sharpening the image:');
+    disp('we have tried two ways of sharpening the image:');
     disp('1)by using high pass filter:');
-    disp('  1-1) HPF+orginal.');
-    disp('  1-2) HPF(HPF+orginal)+orginal.');
-    disp('  1-3) HPF(HPF+orginal)+(HPF+original.');
     disp('2) or we could simply use sharpen function we have written in HW3');
-    disp('In our eyes,The sharpen seemed to work better.But PSNR suggests the 1-1 actually is the better one!');
-    disp('so we have decided to show you both of the resullts');
-    
-    
-
+    disp('In this code, we are showing you the denoised image by using HPF and then bilateral filtering.');
+    disp('so what we did is the following:');
+    disp('We have performed HPF several times,each time we have added the result to the image,');
+    disp(' and then applied the next HPF on the modified image.');
+    disp('We have performed the HPF filtering four times.');
+    disp('The result was very close to what should it be, but it was noised!');
+    disp('So we used bilateral filtering to denoise it and the result was stunning!');
     disp('figure(1) - Original Noisy image');
-    disp('figure(2) - Cleaned Image by HPF');
-    disp('figure(3) - Cleaned Image by shrapen function from hw3');  
-
+    disp('figure(2) - The Cleaned Image by HPF and bilateral filtering');
+    
     imA = readImage('stroller.tif');
     showImage(imA);
-    imO = readImage('stroller2.tif');
-imC = sharpen(imA,2,2,5);
-    Psnr4 = calcPSNR(imC,imO);
-   disp(Psnr4);
-
-    showImage(imC);
+   
+    % applying HPF on imA
+    
     F=fft2(imA);
     H=circle(size(imA),50);
     H = ifftshift(H);
     f = ifft2(F.*(H/255),'symmetric');
-    showImage(imA +f);
-    Psnr1 = calcPSNR((imA+f),imO);
-    disp(Psnr1);
-   
     
-    imA = imA+f;
-F=fft2(imA);
-H=circle(size(imA),20);
-H = ifftshift(H);
-f = ifft2(F.*(H/255),'symmetric');
-showImage(imA +f);
-Psnr3 = calcPSNR((imA+f),imO);
-   disp(Psnr3);
+    % f now is the result of applying HPF on imA
+    
+    imA = imA+f;%we are sharpening imA by adding f to it => imA = imA+HPF(imA)
+    
+    % applying HPF on the modified imA
+    
+    F=fft2(imA);
+    H=circle(size(imA),20);
+    H = ifftshift(H);
+    f = ifft2(F.*(H/255),'symmetric');
+    
+    % f now is the result of applying HPF on imA
+
+    imA = imA+f;%we are sharpening imA by adding f to it => imA = imA+HPF(imA)= imA+HPF(imA)+HPF(imA+HPF(imA))
+    
+    F=fft2(imA);
+    H=circle(size(imA),30);
+    H = ifftshift(H);
+    f = ifft2(F.*(H/255),'symmetric');
+    
+    % f now is the result of applying HPF on imA
+    
+    imA = imA+f;%we are sharpening imA by adding f to it => imA = imA+HPF(imA)= imA+HPF(imA)+HPF(imA+HPF(imA))+HPF(imA+HPF(imA)+HPF(imA+HPF(imA)))
+    
+    % doing one more HPF
+    F=fft2(imA);
+    H=circle(size(imA),40);
+    H = ifftshift(H);
+    f = ifft2(F.*(H/255),'symmetric');
+    imG=imA+f;%imG = imA+f=imA+HPF(imA)+HPF(imA+HPF(imA))+HPF(imA+HPF(imA)+HPF(imA+HPF(imA)))+HPF(imA+HPF(imA)+HPF(imA+HPF(imA))+HPF(imA+HPF(imA)+HPF(imA+HPF(imA))))
+
+    clean=bilat(imG,4,9,9); %imG is a little bit noised from the whole filters we have done, so we denoise it using bilateral filtering
+    showImage(clean);
    
-    imA = imA+f;
-F=fft2(imA);
-H=circle(size(imA),30);
-H = ifftshift(H);
-f = ifft2(F.*(H/255),'symmetric');
-showImage(imA +f);
-Psnr5 = calcPSNR((imA+f),imO);
-   disp(Psnr5);
-   imA = imA+f;
-F=fft2(imA);
-H=circle(size(imA),40);
-H = ifftshift(H);
-f = ifft2(F.*(H/255),'symmetric');
-showImage(imA +f);
-Psnr5 = calcPSNR((imA+f),imO);
-   disp(Psnr5);
-   showImage(imO);
+    writeImage(clean,'cleanStroller');
+   
 end
