ca04379c752e80b90d821655097b15972658d1fa
WaseemWeshahi <41228348+WaseemWeshahi@users.noreply.github.com>
2019-01-08 12:11:11 +0200

    Laplacian finished, but..
    Let's just pretend that upSample works well, we'll figure it out later eventually.

--- Assignment5/laplacPyr.m
+++ Assignment5/laplacPyr.m
@@ -1,6 +1,9 @@
 function    L = laplacPyr(img,levels)
-    GaussPy = gaussPyr(img,levels);
+    L = cell(1,levels);
+    G = gaussPyr(img,levels+1);
     
-    for
+    for i = levels:1
+        L{i} = G{i} - upSample(G{i+1});
+    end
 
 end
